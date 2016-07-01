6abfba681beb03b3bec443644457883c2c6614ab
BridgeIt <bridgeit@vidyas-mac-mini.local>
2016-07-01 11:46:15 +0530

    Initial Commit

---
+++ IPL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPL
+//
+//  Created by BridgeIt on 01/07/16.
+//  Copyright © 2016 bridgelabz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
